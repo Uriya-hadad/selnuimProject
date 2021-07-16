650462b9aacc39bf60c6f5fa98df44a9b19f8d4f
uriya <ooria47@gmail.com>
2021-07-16 13:43:28 +0300

    Extracting strings to an external class

---
+++ src/main/java/Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    public static final String CAMPUS_URL = "https://www.aac.ac.il/";
+    public static final String PORTAL_URL_CSS_PATTERN = "a[href='https://portal.aac.ac.il']";
+    public static final String USER_ID_ATTRIBUTE = "#Ecom_User_ID";
+    public static final String PASSWORD_ID_ATTRIBUTE = "#Ecom_Password";
+    public static final String SUBMIT_BUTTON_ID_ATTRIBUTE = "#wp-submit";
+    public static final String MENU_ID_ATTRIBUTE = "#action-menu-toggle-1";
+    public static final String MOODLE_LOGOUT_CSS_PATTERN = "a[data-title = 'logout,moodle']";
+    public static final String PORTAL_LOGOUT_CSS_PATTERN = "a[href='https://portal.aac.ac.il/AGLogout']";
+    public static final String COURSE_LINK_CSS_PATTERN = "a[class = 'aalink coursename']";
+    public static final String DISMISS_DEBT_ID_ATTRIBUTE = "#debtdismissmodal";
+    public static final String MOODLE_LOGIN_CSS_PATTERN = "a[href='https://moodle.aac.ac.il/login/index.php']";
+
+}
